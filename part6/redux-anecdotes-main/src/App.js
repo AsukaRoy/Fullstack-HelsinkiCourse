f614b3be7377f748a1dd6fb8804c1e04976d5d7a
AsukaRoy <Firo.Y2h@gmail.com>
2021-08-16 14:12:00 +0300

    Part 6
    finish 6.15-6.18

--- part6/redux-anecdotes-main/src/App.js
+++ part6/redux-anecdotes-main/src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import anecdoteService from "./services/anecdotes";
+
 import { initializeAnecdotes } from "./reducers/anecdoteReducer";
 import Notification from "./components/Notification";
 import AnecdoteList from "./components/AnecdoteList";
@@ -9,10 +9,7 @@ import AnecdoteForm from "./components/AnecdoteForm";
 const App = () => {
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    anecdoteService
-      .getAll()
-      .then((anecdotes) => dispatch(initializeAnecdotes(anecdotes)));
+  useEffect(() => {dispatch(initializeAnecdotes());
   }, [dispatch]);
 
   return (
